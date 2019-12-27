bd093cd1ceb0571ac30436a67398b745cbff3cfa
Venkateshwaran <venkateshwarant@gmail.com>
2019-12-27 15:42:14 +0100

    Katalon test cases

---
+++ Katalon/Test Suites/ParallelExecution.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ParallelExecution</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>a8f86f31-7054-41c8-90c1-b2a8a4e28ca0</testSuiteGuid>
+   <testCaseLink>
+      <guid>68401623-ce6a-406e-8760-316d7862d8b4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ef5fb7a0-4c06-411d-b08e-d66aea724c29</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test5</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6e5bb744-9f8b-4674-be53-482002295bf8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test4</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>46df6737-c97a-45a0-b697-871b50e098e5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test3</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>464dce77-27ec-45cd-9e19-a6bda8fcaae3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
